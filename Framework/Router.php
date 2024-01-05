7fa04dae37d6f815d53ce8fad31fbd80acf79ab6
Алексей Бельченков <u608110@gmail.com>
2024-01-05 16:51:43 +0400

    Delete listings

--- Framework/Router.php
+++ Framework/Router.php
@@ -86,6 +86,12 @@ class Router
     {
         $request_method = $_SERVER['REQUEST_METHOD'];
 
+        // Check for _method input
+        if ($request_method === 'POST' && isset($_POST['_method'])) {
+            // Override the request method with the value of _method
+            $request_method = strtoupper($_POST['_method']);
+        }
+        
         foreach ($this->routes as $route) {
             // Split the current URI into segments
             $uri_segments = explode('/', trim($uri, '/'));
--- routes.php
+++ routes.php
@@ -9,5 +9,6 @@ $router->get('/', 'HomeController@index');
 $router->get('/listings', 'ListingController@index');
 $router->get('/listings/create', 'ListingController@create');
 $router->get('/listing', 'ListingController@show');
-$router->get('/listing/{id}', 'ListingController@show');
+$router->get('/listings/{id}', 'ListingController@show');
 $router->post('/listings', 'ListingController@store');
+$router->delete('/listings/{id}', 'ListingController@destroy');
