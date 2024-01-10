a3a3f60fa7a08d7d51b141e02e80ea10551ef830
Алексей Бельченков <u608110@gmail.com>
2024-01-10 13:00:35 +0400

    logout

--- App/controllers/UserController.php
+++ App/controllers/UserController.php
@@ -126,4 +126,21 @@ class UserController
 
         redirect('/');
     }
+
+
+    /**
+     * Logout a user and kill session
+     *
+     * @return void
+     */
+    public function logout(): void
+    {
+        Session::clearAll();
+
+        $params = session_get_cookie_params();
+
+        setcookie('PHPSESSID', '', time() - 86400, $params['path'], $params['domain']);
+
+        redirect('/');
+    }
 }
