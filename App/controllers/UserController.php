1589cb8c46bdf434d0f2988ee870e1de2f1417a1
Алексей Бельченков <u608110@gmail.com>
2024-01-10 12:47:36 +0400

    Session class get user

--- App/controllers/UserController.php
+++ App/controllers/UserController.php
@@ -5,6 +5,7 @@ namespace App\Controllers;
 use Exception;
 use Framework\Database;
 use Framework\Validation;
+use Framework\Session;
 
 class UserController
 {
@@ -43,6 +44,7 @@ class UserController
      * Store user in database
      *
      * @return void
+     * @throws Exception
      */
     public function store(): void
     {
@@ -110,6 +112,17 @@ class UserController
         ];
 
         $this->db->query('INSERT INTO users (name, email, city, state, password) VALUES (:name, :email, :city, :state, :password)', $params);
+
+        // Get new user ID
+        $userId = $this->db->conn->lastInsertId();
+
+        Session::set('user', [
+            'id' => $userId,
+            'name' => $name,
+            'email' => $email,
+            'city' => $city,
+            'state' => $state
+        ]);
 
         redirect('/');
     }
